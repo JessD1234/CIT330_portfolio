356ab545f5f22b11c9a4512bbd98874e9907922f
JessD1234 <46542907+JessD1234@users.noreply.github.com>
2020-09-16 14:40:57 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var weekNav = document.querySelector(".week-navigation");
 
-var weekLabels = [ "Week 1", "Week 2", "Week 3", "Week 4", "Week 5"
+var weekLabels = [ "Week 1", "Week 2", "Week 3", "Week 4", "Week 5",
 ];
 var weekLinks = ["week1.html", "week2.html", "week3.html", "week4.html", "week5.html",];
 
