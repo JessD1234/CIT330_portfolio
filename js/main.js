7b9f8dc59e70c8dcd94fafe34d11109be8a1c236
JessD1234 <46542907+JessD1234@users.noreply.github.com>
2020-09-16 16:02:55 -0600

    Added more to notes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 var weekNav = document.querySelector(".week-navigation");
 
-var weekLabels = [ "Week 1", "Week 2", "Week 3", "Week 4", "Week 5",
-];
-var weekLinks = ["week1.html", "week2.html", "week3.html", "week4.html", "week5.html",];
+var weekLabels = [ "Week 1", "Week 2", "Week 3", "Week 4", "Week 5",];
+var weekLinks = ["week1/week1.html", "week2/week2.html", "week3/week3.html", "week4/week4.html", "week5/week5.html",];
 
-var weekLine = document.createElement ("li");
+//var weekLine = document.createElement ("li");
 //weekLine.textContent = "Week 1":
 
-var a = document.createElement ("a");
+/*var a = document.createElement ("a");
 var link = document.createTextNode ("Week 1");
 a.title = "Week 1";
 a.href = "week1/week1.html"
 
 a.appendChild(link);
 weekLine.appendChild(a);
-weekNav.appendChild(weekLine);
+weekNav.appendChild(weekLine);*/
+
+for (i=0; i < weekLabels.length; i++) {
+    var weekLine = document.createElement ("li");
+    var a = document.createElement ("a");
+    var link = document.createTextNode (weekLabels[i]);
+    a.title = weekLabels[i];
+    a.href = weekLinks[i];
+
+    a.appendChild(link);
+    weekLine.appendChild(a);
+    weekNav.appendChild(weekLine);
+}
