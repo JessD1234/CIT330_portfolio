1258a044e4a2e620b12649e919962fe1bbadb886
JessD1234 <46542907+JessD1234@users.noreply.github.com>
2020-09-16 14:26:05 -0600

    Adding code and testing Javascript

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var weekNav = document.getElementById ("week-navigation").innerHTML;
+
+var weekLinks = [
+    { label = "Week 1", url="week1.html" },
+    { label = "Week 2", url="week2.html" },
+    { label = "Week 3", url="week3.html" },
+    { label = "Week 4", url="week4.html" },
+    { label = "Week 5", url="week5.html" },
+    { label = "Week 6", url="week6.html" },
+    { label = "Week 7", url="week7.html" },
+    { label = "Week 8", url="week8.html" },
+    { label = "Week 9", url="week9.html" },
+    { label = "Week 10", url="week10.html" },
+    { label = "Week 11", url="week11.html" },
+    { label = "Week 12", url="week12.html" },
+    { label = "Week 13", url="week13.html" },
+    { label = "Week 14", url="week14.html" }
+];
+
+var weekLine = document.createElement ("li");
+weekLine.textContent = "Week 1";
+
+weekLine.appendChild(weekNav);
