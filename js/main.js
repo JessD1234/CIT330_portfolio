ff7e429d26e3bea301d533fcfccbebe199088548
JessD1234 <46542907+JessD1234@users.noreply.github.com>
2020-09-16 14:58:39 -0600

    Adding new pages, finishing Javascript basic layout

--- js/main.js
+++ js/main.js
@@ -5,6 +5,13 @@ var weekLabels = [ "Week 1", "Week 2", "Week 3", "Week 4", "Week 5",
 var weekLinks = ["week1.html", "week2.html", "week3.html", "week4.html", "week5.html",];
 
 var weekLine = document.createElement ("li");
-weekLine.textContent = "Week 1";
+//weekLine.textContent = "Week 1":
 
-weekLine.appendChild(weekNav);
+var a = document.createElement ("a");
+var link = document.createTextNode ("Week 1");
+a.title = "Week 1";
+a.href = "week1/week1.html"
+
+a.appendChild(link);
+weekLine.appendChild(a);
+weekNav.appendChild(weekLine);
